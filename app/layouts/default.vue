74361f3cdecbd6020c7fa4270b762e683ef0c538
mya-ake <make.app.ts@gmail.com>
2018-02-18 20:11:10 +0900

    add footer

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
-  <main>
-    <nuxt/>
-  </main>
+  <div>
+    <main>
+      <nuxt/>
+    </main>
+    <footer>
+      <small>&copy; 2018 <a href="https://mya-ake.com/">mya-ake</a></small>
+    </footer>
+  </div>
 </template>
 
 <style lang="scss" scoped>
